03c4256fcd44d37106a80f8a39924ea3a6f40e45
nineis7 <nineis@163.com>
2023-03-22 13:12:45 +0000

    update

---
+++ workloads/__init__.py
@@ -0,0 +1,9 @@
+#from .Swin_Transformer.models import SwinTransformer
+#from .Swin_Transformer import create_logger
+#from .Swin_Transformer import load_pretrained
+
+from .GPT.mingpt.model import GPT, GPTConfig
+from .GPT.mingpt.utils import sample
+
+#from .Switch_Transformer.labml_nn.transformers.switch import Configs
+#from .Switch_Transformer.labml_nn.transformers.switch import switch_transformer
